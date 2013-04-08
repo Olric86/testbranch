f96fff957a135a8ab2496eba042482386b0a8358
Carlo <carlo.pastorino@live.com>
2013-04-08 22:27:07 +0200

    Added version indicator in the bottom-right corner

--- public/js/app.js
+++ public/js/app.js
@@ -5,8 +5,27 @@ if (this.App === undefined)
 
 App.Core = (function(){
   var version = 0.1;
+  
+  var _initEvents = function()
+  {
+
+  };
+
+  var _init = function()
+  {
+    _initEvents();
+
+    var div = document.createElement("div");
+    div.id = 'versionInfo';
+    div.appendChild(document.createTextNode(version));
+
+    document.body.appendChild(div);
+    div.style.display = 'block';
+  };
+
+  document.addEventListener("DOMContentLoaded", _init, false);
 
   return {
     getVersion: function() { return version; }
-  }
+  };
 })();
